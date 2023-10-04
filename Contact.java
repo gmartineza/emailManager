c2820211c9f7e4d39de3f5159181992da1016165
gmartineza <68785609+gmartineza@users.noreply.github.com>
2023-10-04 15:39:19 -0300

    add contact constructors

--- Contact.java
+++ Contact.java
@@ -2,10 +2,33 @@ public class Contact {
     String name;
     String surname;
     String emailAddress;
+
+    public Contact(String name, String surname, String emailAdress){
+        this.name = name;
+        this.surname = surname;
+        this.emailAddress = emailAdress;
+    }
+
+    public Contact(String emailAdress){
+        this.emailAddress = emailAdress;
+    }
+
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public void setEmailAdress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+    public String getEmailAdress() {
+        return emailAddress;
     }
 }
