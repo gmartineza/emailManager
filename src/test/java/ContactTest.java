0604673ad850b0d2c302d76b264414ab7a39ffed
gmartineza <gabymartinezalarcon@gmail.com>
2023-10-05 09:59:34 -0300

    refactor contact instanciation test

--- src/test/java/ContactTest.java
+++ src/test/java/ContactTest.java
@@ -3,8 +3,11 @@ import static org.junit.Assert.*;
 import ucp.gmartineza.emailmanager.*;
 public class ContactTest {
     @Test
-    public void InstantiateContactTest(){
-    Contact contact = new Contact("gabymartinezalarcon@gmail.com", "Gabriel", "Martinez");
-    assertNotNull(contact);
+    public void InstantiateContactsTest(){
+    Contact fullContact = new Contact("mrbeast@yahoo.org", "Michael", "Jackson");
+    Contact emailOnlyContact = new Contact("mrbeast@yahoo.org");
+    assertNotNull(fullContact);
+    assertNotNull(emailOnlyContact);
+    assertNotEquals(fullContact, emailOnlyContact);
     }
 }
